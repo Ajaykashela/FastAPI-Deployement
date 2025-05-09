8fa33a7f15389e4973ae5559eeb8654f59949103
Conga_akashela <akashela@conga.com>
2025-05-09 15:46:28 +0530

    just FASTAPI NOW

--- app.py
+++ app.py
@@ -1,18 +1,7 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-from transformers import pipeline
 
 app = FastAPI()
 
-classifier = pipeline(
-    "text-classification",
-    model="nlptown/bert-base-multilingual-uncased-sentiment"
-)
-
-class InputText(BaseModel):
-    text: str
-
-@app.post("/predict")
-def predict(input: InputText):
-    result = classifier(input.text)
-    return {"result": result}
+@app.get("/")
+def read_root():
+    return {"message": "🚀 FastAPI is working!"}
