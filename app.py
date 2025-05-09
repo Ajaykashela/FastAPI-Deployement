cd2f041e89e1ce86404b688504d142888f7aeace
Conga_akashela <akashela@conga.com>
2025-05-09 11:19:05 +0530

    updated as per my model

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-from TextGen import app
+from fastapi import FastAPI
+from pydantic import BaseModel
+from transformers import pipeline
+
+app = FastAPI()
+
+classifier = pipeline("text-classification", model="ajaykashela/distilbert_email_classifier_v2")
+
+class InputText(BaseModel):
+    text: str
+
+@app.post("/predict")
+def predict(input: InputText):
+    result = classifier(input.text)
+    return {"result": result}
